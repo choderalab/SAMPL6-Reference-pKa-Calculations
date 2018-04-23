1b1b3d1e1eeb4329e23eda82eba710b7ee842eb4
Bas Rustenburg <bas.rustenburg@choderalab.org>
2018-04-23 17:35:03 -0400

    Update scripts to fix bugs

--- generate_all_input.sh
+++ generate_all_input.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/bash
 
+schropy='/data/chodera/schrodinger/schrodinger2017-4-advanced/run python'
+
 # quit on first error 
 set -e
 mkdir -p "jaguar_input_files"
 
 cd microstates
 
-for filename in microstates/*.maegz
+for filename in *.maegz
 do
     # use alias to schrodingers python and run the script.
-    schropy make_jaguar_input2 ${filename}
+    ${schropy} ../make_jaguar_input2.py ${filename}
 done
 
 cd ..
