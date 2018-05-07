ddfb81c109153dec7ca83966686b1529a77cd33c
Bas Rustenburg <bas.rustenburg@choderalab.org>
2018-05-07 13:18:33 -0400

    Check in script changes.

--- generate_all_input.sh
+++ generate_all_input.sh
@@ -8,7 +8,7 @@ mkdir -p "jaguar_input_files"
 
 cd microstates
 
-for filename in *.maegz
+for filename in *.mae
 do
     # use alias to schrodingers python and run the script.
     ${schropy} ../make_jaguar_input2.py ${filename}
